d462bbd92f2c9e029d6bd622f36e45366e1b351e
Yashwanth Kumar <kyashwanth831@gmail.com>
2025-12-05 12:42:55 +0530

    Fix socket URL by removing trailing slash

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,7 @@ import {
   Spacer
 } from "@chakra-ui/react";
 
-const socket = io("https://chatspac-app-backend.onrender.com/");
+const socket = io("https://chatspac-app-backend.onrender.com");
 
 const colors = ["blue", "green", "red", "purple", "orange", "teal", "pink", "yellow"];
 
